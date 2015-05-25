fce016dd36880633c78653ae150d97e75199fba5
Yadu <yadu.k@zebra.com>
2015-05-25 15:57:17 +0530

    Added 2nd program

---
+++ src/2-MLO-LedTrafficSignal/README.txt
@@ -0,0 +1,27 @@
+2-MLO-LedTrafficSignal:
+======================
+
+This is a bare metal ARM assembly program to turn on all 4 usr leds one by one in a F1/grand-prix
+race start signal pattern.
+A slight improvement over the program in "1"
+
+The actual binary generated will be prefixed with a TOC and GP header using the 'signgp' utility.
+This final binary is called MLO - Minimal loader.
+The actual purpose of an MLO is to chain load the second stage bootloader which will futher boot the kernel.
+But here we're just trying to boot the BBB using our MLO from the external u-sdcard.
+The MLO just acts as a container for bare metal assembly code.
+
+To build: 
+1) Make sure you have built the signgp utility first.
+2) Edit build.sh script to reflect your toolchain path
+2) ./build.sh mlo
+
+To run:
+This file has to be put onto an sd-card in raw format as described in the TI AM335x TRM - Sec 26.1.7.5.5
+1) Get hold of a u-sdcard and card reader.
+2) Delete all partitions on it and ensure there is only free space.
+3) sudo dd if=/dev/zero of=/dev/sdX bs=512K count=1 --- clear out first 512k bytes of the disk with zeroes
+4) sudo dd if=MLO of=/dev/sdX count=1 seek=1 conv=notrunc bs=128k --- write the MLO at an offset of 128k
+5) Let BBB boot into its default OS and insert the usd.
+6) While holding the boot (S2) button remove power cable/USB cable and re-insert power while continuing to hold down S2.
+7) You should see the leds come on one by one and go off and repeat the pattern.
---
+++ src/2-MLO-LedTrafficSignal/mlo.s
@@ -0,0 +1,87 @@
+/*
+Bare metal BeagleBone Black example for turning on usr led's in a grand prix/F1 traffic light fashion.
+*/
+
+.equ CM_PER_GPIO1_CLKCTRL, 0x44e000AC
+.equ GPIO1_RISINGDETECT, 0x44e00148
+.equ GPIO1_OE, 0x4804C134
+.equ GPIO1_SETDATAOUT, 0x4804C194
+.equ GPIO1_CLEARDATAOUT, 0x4804C190
+.equ GPIO1_DATAOUT, 0x4804C13C
+.equ LOOP_CTR, 9615384
+
+_start:
+    mrs r0, cpsr
+    bic r0, r0, #0x1F @ clear mode bits
+    orr r0, r0, #0x13 @ set SVC mode - supervisor mode
+    orr r0, r0, #0xC0 @ disable FIQ and IRQ
+    msr cpsr, r0
+
+    /* set clock for GPIO1, TRM 8.1.12.1.31 */
+    ldr r0, =CM_PER_GPIO1_CLKCTRL
+    mov r1, #0x02
+    str r1, [r0]
+
+	/*set logical 0 on gpio1-21,22,23,24 pins to turn off all led's first*/
+    ldr r0, =GPIO1_DATAOUT
+    mov r1, #0
+    str r1, [r0]
+
+    /* set pins 21,22,23,24  for output TRM 25.3.4.3 */
+    ldr r0, =GPIO1_OE
+    ldr r1, [r0]
+    bic r1, r1, #(0x0F<<21)
+    str r1, [r0]
+    
+    ldr r0, =GPIO1_DATAOUT    
+START:
+	/*branch with link, addr. of the instr. next to the branch is stored in LR before branching*/
+	bl DELAY
+
+	/*turn on led 4, i.e, the 24th gpio1 pin => 24th bit in the reg*/
+    ldr r1, =(0xFF000000)
+    str r1, [r0]
+    bl DELAY
+    
+    /*turn on led 3*/
+    lsr r1, #1
+    str r1, [r0]
+    bl DELAY
+    
+    /*turn on led 2*/
+    lsr r1, #1
+    str r1, [r0]
+    bl DELAY
+    
+    /*turn on led 1*/
+    lsr r1, #1
+    str r1, [r0]
+    bl DELAY
+    
+    //clear all leds
+    mov r1, #0
+    str r1, [r0]
+        
+	b START 
+    
+    /*Delay of 1s - some dummy instructions executed in a tight loop
+    ALU instructions take 1 cycle to exec, branch takes 3 cycles -- see Sec 6.3 in ARM system developers guide
+    Extrapolate that to the 1ghz AM3358 processor on BBB for a loop count to get a 1s delay*/
+DELAY:
+	ldr r8, =LOOP_CTR
+DELAY_LOOP1:
+    add r2, r3, r4
+    add r5, r6, r7
+    add r2, r3, r4
+    add r5, r6, r7
+    add r2, r3, r4
+    add r5, r6, r7
+    add r2, r3, r4
+    add r5, r6, r7      
+    sub r8, #1
+    cmp r8, #0
+	bne DELAY_LOOP1
+   
+    /*return to caller*/
+    mov pc, lr
+    
