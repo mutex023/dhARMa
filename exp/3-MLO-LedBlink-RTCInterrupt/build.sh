6f9b7a7a1458228cf9eef28af8f4a11cbe822761
ykodiche <ykodiche@broadcom.com>
2017-02-13 11:21:21 +0530

    added toolchain and experimental folder

---
+++ exp/3-MLO-LedBlink-RTCInterrupt/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  
+#export PATH=$PATH:"/home/svtuser/Projects/dhARMa/toolchain/gcc-arm-none-eabi-4_9-2015q1/bin"
+export PATH=$PATH:"/home/svtuser/Projects/dhARMa/toolchain/arm-2014.05/bin/"
+
+arm-none-eabi-as $1.s -o $1.o
+arm-none-eabi-objcopy $1.o $1.bin -O binary
+#arm-none-eabi-objdump -SD $1.o
+
+../../bin/signgp ./$1.bin
+mv $1.bin.ift MLO
