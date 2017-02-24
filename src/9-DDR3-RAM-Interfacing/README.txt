afce802a1d7f82961179faaa961a55b9c3a372b5
mutex023 <mutex023@dharma.org>
2017-02-24 16:35:43 +0530

    prog 9 now works, added latest revO TRM of AM335x, updated url list

--- src/9-DDR3-RAM-Interfacing/README.txt
+++ src/9-DDR3-RAM-Interfacing/README.txt
@@ -1,17 +1,12 @@
-8-C-Runtime-Led
+9-DDR3-RAM-interfacing
 ===============================
-We now switch to bare metal C programming.
-For this the initial setup of the stack and BSS segment is done by the assembly code in start.s
-and then jumps to C code in main.c
-The BSS segment has to be zeroed out.
-Note that in bare metal mode when compiling we have to mention some options to the gcc compiler:
--Wall = turn on all warnings, optional but very useful !
--O2 = optimization level 2, this is only optional
--nostdlib = do not use standard C libraries while compiling, because in bare metal there is no C runtime available
--ffreestanding = specifies that there is no underlying OS and c-runtime when the binary is run
--fstack-usage -Wstack-usage=256 : for ensuring a stack size of 256 is used.
-
-The 'memmap' file is a linker script, required to specify the load address and BSS segments.
+Time to use the RAM the BBB !
+We build on top of prog 8 and initialize the DDR3 RAM on BBB.
+Then we perform a simple test of writing to the RAM and reading back to check if
+the RAM is functioning properly.
+This prog is full of setting up various RAM related registers and EMIF registers properly.
+Most values are hardcoded.
+The sequence of registers to program and their values have been copied from TI's starterware code.
 
 To build: 
 1) Make sure you have built the signgp utility first.
@@ -28,5 +23,5 @@ This file has to be put onto an sd-card in raw format as described in the TI AM3
 4) sudo dd if=/dev/zero of=/dev/sdX bs=512K count=1 --- clear out first 512k bytes of the disk with zeroes; [usually in BBB you can replace 'sdX' by 'mmcblk0']
 5) sudo dd if=/root/MLO of=/dev/sdX count=1 seek=1 conv=notrunc bs=128k --- write the MLO at an offset of 128k
 6) While holding the boot (S2) button remove power cable/USB cable and re-insert power while continuing to hold down S2.
-7) You should see the usr3 led turn on
+7) You should see the usr1 and usr2 led turn on if RAM init. is successful, on failure usr3 led should light up.
 
--- src/9-DDR3-RAM-Interfacing/main.c
+++ src/9-DDR3-RAM-Interfacing/main.c
@@ -1,9 +1,9 @@
 /* Bare metal C program to initialize the onboard 512MB MIcron DDR3 RAM
-* on BBB. The usr0 led will be turned on if init is successful.
+* on BBB. The usr1 and usr2 led will be turned on if init is successful.
 * The code to init the RAM is taken from TI Starterware code
 * (~/bootloader/src/armv7a/am335x/bl_platform.c - DDRPLLInit(), EMIFInit(), DDR3PhyInit() & DDR3Init() )
 * Initializing RAM involves setting different values to different registers
-* and involves complex terminology like Phase Locked Loops (PLL),
+* and involves complex terminology like Phase Locked Loops (PLL), VTP (Voltage, temp, process)
 * ODT (On die Termination), slave ratios, CAS/RAS (column/row addr strobe) etc..,
 * It is best to understand as much as possible and if you find the going tough
 * skip the things you don't understand :). As long as we are able to use the RAM for
@@ -14,11 +14,19 @@
 
 #include "../common/bbb_hal.h"
 
+void usr_led_on(unsigned char led_num)
+{
+	unsigned int val = 0;
+	
+	val = 0x1 << (led_num + 21);
+	WRITEREG32(GPIO1_SETDATAOUT, val);
+}
+
 void init_ddr_pll()
 {
 	unsigned int val = 0;
 	
-	/* put the DPLL in MN bypass mode - no idea what this means - TRM 8.1.12.2.38 */
+	/* put the DPLL in MN bypass mode - TRM 8.1.12.2.38 */
 	val = READREG32(CM_WKUP_REGS_BASE + CM_WKUP_CM_CLKMODE_DPLL_DDR_OFFSET);
 	val &= ~0x7;
 	val |= 0x4;
@@ -68,23 +76,14 @@ void init_emif()
 	WRITEREG32(CM_PER_REGS_BASE + CM_PER_EMIF_CLKCTRL_OFFSET, val);
 	
 	/* wait for the clocks EMIF_GCLK & L3_GCLK to go active - TRM 8.1.12.1.4 */
-	while (READREG32(CM_PER_REGS_BASE + CM_PER_L3_CLKSTCTRL_OFFSET) & 0x14 != 0x14)
+	while ((READREG32(CM_PER_REGS_BASE + CM_PER_L3_CLKSTCTRL_OFFSET) & 0x14) != 0x14)
 		;
 }
 
-int init_ddr3_ram()
+void init_ddr_phy()
 {
 	unsigned int val = 0;
-	
-	/* initialize the DDR3 PLL */
-	init_ddr_pll();
-	
-	/* enable the CM_WKUP clock control module - TRM 8.1.12.2.2 */
-	WRITEREG32(CM_WKUP_REGS_BASE + CM_WKUP_CONTROL_CLKCTRL_OFFSET, 0x2);
-	
-	/* initialize the External mem interface EMIF */
-	init_emif();
-	
+
 	/* enable VTP (Voltage, Temp, Process) control, TRM - 9.3.54 */
 	val = READREG32(CTRL_MODULE_REG_BASE + CTRL_MODULE_VTP_CTRL_OFFSET);
 	val |= 0x40;
@@ -141,7 +140,89 @@ int init_ddr3_ram()
 
 }
 
-void main()
+void init_ddr3_ram()
+{
+	unsigned int val = 0;
+	
+	/* initialize the DDR3 PLL */
+	init_ddr_pll();
+	
+	/* enable the CM_WKUP clock control module - TRM 8.1.12.2.2 */
+	WRITEREG32(CM_WKUP_REGS_BASE + CM_WKUP_CONTROL_CLKCTRL_OFFSET, 0x2);
+	
+	/* initialize the External mem interface EMIF */
+	init_emif();
+	
+	/* initialize ddr VTP and phy regs */
+	init_ddr_phy();
+
+	/* set IO for DDR3 - TRM 9.3.53 */
+	val = READREG32(CTRL_MODULE_REG_BASE + CTRL_MODULE_DDR_IO_CTRL_OFFSET);
+	val &= DDR3_CTRL_MODULE_DDR_IO_CTRL_VAL;
+	WRITEREG32(CTRL_MODULE_REG_BASE + CTRL_MODULE_DDR_IO_CTRL_OFFSET, val);
+
+	/* set clock enable to be controlled by EMIF/DDR - TRM 9.3.77 */
+	val = READREG32(CTRL_MODULE_REG_BASE + CTRL_MODULE_DDR_CKE_CTRL_OFFSET);
+	val |= 0x1;
+	WRITEREG32(CTRL_MODULE_REG_BASE + CTRL_MODULE_DDR_CKE_CTRL_OFFSET, val);
+
+	/* set read latency and dynamic pwr down in EMIF - TRM 7.3.5.33/34 */
+	WRITEREG32(EMIF_REG_BASE + EMIF_DDR_PHY_CTRL_1_OFFSET, DDR3_DDR_PHY_CTRL_1_VAL);
+	WRITEREG32(EMIF_REG_BASE + EMIF_DDR_PHY_CTRL_1_SHDW_OFFSET, DDR3_DDR_PHY_CTRL_1_VAL);
+
+	/* configure SDRAM clk cycle values - TRM 7.3.5.7-12 */
+	WRITEREG32(EMIF_REG_BASE + EMIF_SDRAM_TIM_1_OFFSET, DDR3_EMIF_SDRAM_TIM_1_VAL);
+	WRITEREG32(EMIF_REG_BASE + EMIF_SDRAM_TIM_1_SHDW_OFFSET, DDR3_EMIF_SDRAM_TIM_1_VAL);
+	WRITEREG32(EMIF_REG_BASE + EMIF_SDRAM_TIM_2_OFFSET, DDR3_EMIF_SDRAM_TIM_2_VAL);
+	WRITEREG32(EMIF_REG_BASE + EMIF_SDRAM_TIM_2_SHDW_OFFSET, DDR3_EMIF_SDRAM_TIM_2_VAL);
+	WRITEREG32(EMIF_REG_BASE + EMIF_SDRAM_TIM_3_OFFSET, DDR3_EMIF_SDRAM_TIM_3_VAL);
+	WRITEREG32(EMIF_REG_BASE + EMIF_SDRAM_TIM_3_SHDW_OFFSET, DDR3_EMIF_SDRAM_TIM_3_VAL);
+	
+	/* set SDRAM refresh rate - TRM 7.3.5.5/6 */
+	WRITEREG32(EMIF_REG_BASE + EMIF_SDRAM_REF_CTRL_OFFSET, DDR3_EMIF_SDRAM_REF_CTRL_VAL);
+	WRITEREG32(EMIF_REG_BASE + EMIF_SDRAM_REF_CTRL_SHDW_OFFSET, DDR3_EMIF_SDRAM_REF_CTRL_VAL);
+	
+	/* perform ZQ (zero quantum ?) configuration - TRM 7.3.5.29 */
+	WRITEREG32(EMIF_REG_BASE + EMIF_ZQ_CONFIG_OFFSET, DDR3_EMIF_ZQ_CONFIG_VAL);
+	
+	/* configure SDRAM - TRM 7.3.5.3
+	* termination = 1 (RZQ/4)
+	* dynamic ODT = 2 (RZQ/2)
+	* SDRAM drive = 0 (RZQ/6)
+	* CWL = 0 (CAS write latency = 5)
+	* CL = 2 (CAS latency = 5)
+	* ROWSIZE = 5 (14 row bits)
+	* PAGESIZE = 2 (10 column bits)
+	*/
+	WRITEREG32(EMIF_REG_BASE + EMIF_SDRAM_CONFIG_OFFSET, DDR3_EMIF_SDRAM_CONFIG_VAL);
+	
+	/* export the sdram config to EMIF - TRM 9.3.5 */
+	WRITEREG32(CTRL_MODULE_REG_BASE + CTRL_MODULE_EMIF_SDRAM_CONFIG, DDR3_EMIF_SDRAM_CONFIG_VAL);
+}
+
+unsigned char ram_test(unsigned int start_val)
+{
+	int i = 0;
+	unsigned int *p = (unsigned int *)EMIF_DDR3_RAM_START_ADDR;
+	
+	/* write incrementing values to the first 1Kb of RAM and read back */
+	for (i = 0; i < 1024; ++i)
+	{
+		*p = start_val + i;
+		++p;
+	}
+	p = (unsigned int *)EMIF_DDR3_RAM_START_ADDR;
+	for (i = 0; i < 1024; ++i)
+	{
+		if (*p != (start_val + i))
+			return 0;
+		++p;
+	}
+
+	return 1;
+}
+
+void init_led()
 {
 	unsigned int val = 0;
 
@@ -158,9 +239,28 @@ void main()
 
 	/* clear out gpio1-21,22,23,24 pins using data out register first*/
 	WRITEREG32(GPIO1_CLEARDATAOUT, 0x0F<<21);
+}
+
+void main()
+{
+	unsigned int *p = (unsigned int *)EMIF_DDR3_RAM_START_ADDR + 512;
+	init_led();
 	
-	/* turn on usr3 led */
-	WRITEREG32(GPIO1_SETDATAOUT, 0x01<<24);
+	init_ddr3_ram();
+	
+	if(ram_test(0))
+		usr_led_on(1);
+	else
+		usr_led_on(3); //FAIL !
+	
+	/* one more simple RAM test */
+	*p = 3;
+	*(p + 1) = 8;
+	*(p + 2) = (*p) + (*(p + 1));
+	if(*(p + 2) == 11)
+		usr_led_on(2);
+	else
+		usr_led_on(3); //FAIL !
 	
 	while(1);
 }
